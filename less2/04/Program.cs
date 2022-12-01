f0be0f5a129c791c75d51c0c90e06de8e588950b
krava9999 <kosmo@internet.ru>
2022-12-01 22:33:46 +1000

    решение задачи 4.

---
+++ less2/04/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. 
+Console.WriteLine("введите день недели");
+int num = Convert.ToInt32(Console.ReadLine());
+switch (num)
+{
+    case 6:
+    case 7:
+        Console.WriteLine("День недели является выходным");
+        break;
+    default:
+        Console.WriteLine("День недели НЕ является выходным");
+        break;
+}
