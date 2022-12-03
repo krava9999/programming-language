b75e07961095306b438dc6d6727c260e4ef2c041
krava9999 <kosmo@internet.ru>
2022-12-03 19:22:30 +1000

    задача номер три

---
+++ less3/03/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+double result;
+string answer = String.Empty;
+while (i <= num)
+{
+    result = Math.Pow(i, 3);
+    answer = answer + result + ' ';
+    i++;
+}
+Console.WriteLine(answer);
