8fa48876abc771cbf4acf6ac753ba75635c08aac
krava9999 <kosmo@internet.ru>
2022-12-03 16:40:26 +1000

    задача номер один

---
+++ less3/01/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Не использовать строки
+
+Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int leftPart;
+int rigthPart;
+if ((num >= 10000 && num <= 99999) || (num <= -10000 && num >= -99999))
+{
+    leftPart = num / 1000;// Откидываем всю левую часть от центра включительно
+    rigthPart = num % 100;// Откидываем всю правую часть от центра включительно
+    rigthPart = (rigthPart % 10) * 10 + rigthPart / 10; // разворачиваем правую часть
+    if (leftPart == rigthPart)
+    {
+        Console.WriteLine($"число {num} палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"Число  {num} не палиндром  ");
+    }
+
+}
+else
+{
+    Console.WriteLine("Число не пятизначное ");
+
+}
