2e950d7ea7dc4a3e6c1ea82dadcf99ae4a0c9795
krava9999 <kosmo@internet.ru>
2022-12-07 16:07:53 +1000

    задание один, урок 4

---
+++ less4/01/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Реализовать через функции.
+
+void step(int a, int b)
+{
+    int i = 2; int num = a;
+    while (b >= 2)
+    {
+        a *= num;
+        b--;
+    }
+    Console.WriteLine(a);
+}
+
+
+Console.WriteLine("Введите число А");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+step(a, b);
+
+
