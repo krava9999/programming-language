f46f70d6e17455e71bffe2c47c9d28e3f2890245
krava9999 <kosmo@internet.ru>
2022-12-07 16:14:43 +1000

    Задание два, урок 4

---
+++ less4/02/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Реализовать через функции.
+Console.WriteLine("Введите число А");
+int a = Convert.ToInt32(Console.ReadLine());
+sumAllNum(a);
+
+void sumAllNum(int a)
+{
+    int sum = 0;
+    while (a >= 0)
+    {
+        sum += a;
+        a--;
+    }
+    Console.WriteLine(sum);
+}
