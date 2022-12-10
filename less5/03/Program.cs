6ed00f6ba5a31137b5d7827e68f20df400eba3b7
krava9999 <kosmo@internet.ru>
2022-12-10 16:17:03 +1000

    Урок 5. Задача 3

---
+++ less5/03/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+int[] randomArr()
+{
+    Console.WriteLine("Введите размер массива");
+    int size = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите минимальный порог случайных значений");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите максимальный порог случайных значений");
+    int max = Convert.ToInt32(Console.ReadLine());
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(min, max);
+    }
+    Console.Write($"[{string.Join(", ", arr)}] \n");
+    return arr;
+}
+int[] arr = randomArr();
+
+int difference()
+{
+    int maxValue = arr.Max();
+    int minValue = arr.Min();
+    return maxValue - minValue;
+}
+
+Console.Write($"Разность минимального и максимальных значений равна {difference()}");
+
