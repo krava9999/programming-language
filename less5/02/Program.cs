b83533ee6a56ddbd564bdd1e8023884122f5e183
krava9999 <kosmo@internet.ru>
2022-12-10 16:09:13 +1000

    Урок 5. Задача 2

---
+++ less5/02/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+int[] randomArr()
+{
+    Console.WriteLine("Введите размер массива");
+    int size = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите минимальный порог случайных значений");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите максимальный порог случайных значений");
+    int max = Convert.ToInt32(Console.ReadLine());
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(min, max);
+    }
+    Console.Write($"[{string.Join(", ", arr)}] \n");
+    return arr;
+}
+int[] arr = randomArr();
+
+int sumUnEvenPosition(int[] arrMass)
+{
+    int sum = 0;
+    for (int i = 1; i < arrMass.Length; i = i + 2)
+    {
+        sum += arrMass[i];
+    }
+    return sum;
+}
+
+Console.Write($"Cумма элементов, стоящих на нечётных позициях{sumUnEvenPosition(arr)}");
