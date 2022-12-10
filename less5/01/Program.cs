4e07992d6cfb8004420f5f2a3a1175515676aae4
krava9999 <kosmo@internet.ru>
2022-12-10 15:53:12 +1000

    Урок 5. задача 1

---
+++ less5/01/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] randomArr()
+{
+    Console.WriteLine("Введите размер массива");
+    int a = Convert.ToInt32(Console.ReadLine());
+    int[] arr = new int[a];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+    Console.Write($"[{string.Join(", ", arr)}] \n");
+    return arr;
+}
+int[] arr = randomArr();
+
+int countEvenNumsInArr(int[] arrMass)
+{
+    int countEvenNums = 0;
+    for (int i = 0; i < arrMass.Length; i++)
+    {
+        if (arrMass[i] % 2 == 0)
+        {
+            countEvenNums++;
+        }
+    }
+    return countEvenNums;
+}
+
+Console.Write($"Количество четных чисел в массиве {countEvenNumsInArr(arr)}");
