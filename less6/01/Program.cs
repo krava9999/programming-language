d7a1d193adbb1791a484c2c8c22a93478de3d0e1
krava9999 <kosmo@internet.ru>
2022-12-16 16:05:47 +1000

    урок 6. задача1

---
+++ less6/01/Program.cs
@@ -0,0 +1,35 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] newUserArr()
+{
+    Console.WriteLine("Сколько чисел хотите ввести?");
+    int countNums = Convert.ToInt32(Console.ReadLine());
+    int[] arr = new int[countNums];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+
+    }
+    Console.Write($"Ваш массив: [{string.Join(", ", arr)}] \n");
+    return arr;
+
+}
+
+void showAllPlusNums(int[] arr)
+{
+    int countPlusNums = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] <= 0)
+        {
+            countPlusNums++;
+        }
+    }
+
+    Console.Write($"Количество положительных чисел в массиве: {countPlusNums}");
+}
+
+int[] arr = newUserArr();
+showAllPlusNums(arr);
+
