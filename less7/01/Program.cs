a117c08b2d053f9466d89b35d45f83e3317185eb
krava9999 <kosmo@internet.ru>
2022-12-18 00:36:12 +1000

    урок 7. задача 1

---
+++ less7/01/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+double[,] createNewArr()
+{
+    Console.WriteLine("Введите количество строк");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите количество столбцов");
+    int n = Convert.ToInt32(Console.ReadLine());
+    double[,] doubleArr = new double[m, n];
+
+    for (int i = 0; i < doubleArr.GetLength(0); i++)
+    {
+        for (int j = 0; j < doubleArr.GetLength(1); j++)
+        {
+            doubleArr[i, j] = Math.Round(new Random().NextDouble(), 2);
+            Console.Write($"{doubleArr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return doubleArr;
+}
+
+
+double[,] doubleArr = createNewArr();
